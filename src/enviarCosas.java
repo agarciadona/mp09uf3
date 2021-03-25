b841d71be5ce1376ee2582c56f6b3fd2eb617dab
arnau <agarciadona@elpuig.xeill.net>
2021-03-25 19:49:51 +0100

    Initial commit

---
+++ src/enviarCosas.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class enviarCosas {
+    public static void main(String... args) throws IOException {
+        URL url = new URL("https://docs.google.com/forms/u/0/d/e/1FAIpQLScE6sxLFb3BmCmT2TKHQH5ormS0qvjHwO-uTAR8MXaagBvSSQ/formResponse");
+        URLConnection con = url.openConnection();
+        con.setDoOutput(true);
+
+        OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
+        out.write("entry.835030737=" + "");
+        out.write("entry.1616686619=" + "Si");
+        out.close();
+        con.getInputStream();
+
+    }
+}
